a1372799c1cda182e14acb02db03f5cb1dbd172f
Lazybones3 <caorui452@163.com>
2024-03-31 01:41:43 +0000

    update listing_2.3.cpp

--- listings/listing_2.3.cpp
+++ listings/listing_2.3.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 
+// Listing 2.3 Using RAII to wait for a thread to complete
 class thread_guard
 {
     std::thread& t;
@@ -7,8 +8,10 @@ public:
     explicit thread_guard(std::thread& t_):
         t(t_)
     {}
+    // Use the RAII idiom and provide a class that does the join() in its destructor
     ~thread_guard()
     {
+        // join() can be called only once for a given thread of execution
         if(t.joinable())
         {
             t.join();
@@ -41,7 +44,7 @@ struct func
 void do_something_in_current_thread()
 {}
 
-
+// When the execution of the current thread reaches the end of f, the local objects are destroyed in reverse order of construction.
 void f()
 {
     int some_local_state;
