e7edc702fd18c89435eb457a5244fce83dd85e57
Lazybones3 <31243810+Lazybones3@users.noreply.github.com>
2024-04-01 21:53:47 +0800

    Update listing_3.13.cpp

--- listings/listing_3.13.cpp
+++ listings/listing_3.13.cpp
@@ -13,6 +13,7 @@ class dns_cache
 public:
     dns_entry find_entry(std::string const& domain)
     {
+        // find_entry() uses an instance of std::shared_lock<> to protect it for shared, read-only access.
         std::shared_lock<std::shared_mutex> lk(entry_mutex);
         std::map<std::string,dns_entry>::const_iterator const it=
             entries.find(domain);
@@ -21,6 +22,8 @@ public:
     void update_or_add_entry(std::string const& domain,
                              dns_entry const& dns_details)
     {
+		// update_or_add_entry() uses an instance of std::lock_guard<> to provide exclusive access while the table is updated.
+        // not only are other threads prevented from doing updates in a call to update_ or_add_entry(), but threads that call find_entry() are blocked too.
         std::lock_guard<std::shared_mutex> lk(entry_mutex);
         entries[domain]=dns_details;
     }
