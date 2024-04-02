8cbd828f194833b3105d4d3b5e4391d3d347923a
Lazybones3 <31243810+Lazybones3@users.noreply.github.com>
2024-04-02 13:12:23 +0800

    Update listing_4.6.cpp

--- listings/listing_4.6.cpp
+++ listings/listing_4.6.cpp
@@ -1,5 +1,6 @@
 #include <future>
 #include <iostream>
+
 int find_the_answer_to_ltuae()
 {
     return 42;
@@ -8,6 +9,7 @@ int find_the_answer_to_ltuae()
 void do_other_stuff()
 {}
 
+// Listing 4.6 Using std::future to get the return value of an asynchronous task
 int main()
 {
     std::future<int> the_answer=std::async(find_the_answer_to_ltuae);
