bb7671b969386234d24b5ca57c21d01ad8982909
Lazybones3 <31243810+Lazybones3@users.noreply.github.com>
2024-04-02 23:00:36 +0800

    Update listing_4.17.cpp

--- listings/listing_4.17.cpp
+++ listings/listing_4.17.cpp
@@ -1,9 +1,13 @@
 #include <experimental/future>
+
+// Listing 4.17 A simple equivalent to std::async for Concurrency TS futures
 template<typename Func>
 std::experimental::future<decltype(std::declval<Func>()())>
 spawn_async(Func&& func){
     std::experimental::promise<
         decltype(std::declval<Func>()())> p;
+    // use std::experimental::promise to obtain a future, 
+    // and then spawn a new thread running a lambda that sets the promise’s value to the return value of the supplied function
     auto res=p.get_future();
     std::thread t(
         [p=std::move(p),f=std::decay_t<Func>(func)]()
