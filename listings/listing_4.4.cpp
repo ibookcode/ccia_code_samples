bf629a0cc663d7ed607ee0aa80144269def0ea04
Lazybones3 <31243810+Lazybones3@users.noreply.github.com>
2024-04-02 12:46:16 +0800

    Update listing_4.4.cpp

--- listings/listing_4.4.cpp
+++ listings/listing_4.4.cpp
@@ -1,6 +1,8 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+
+// Listing 4.4 Extracting push() and wait_and_pop() from listing 4.1
 template<typename T>
 class threadsafe_queue
 {
@@ -34,6 +36,8 @@ bool more_data_to_prepare();
 void process(data_chunk);
 bool is_last_chunk(data_chunk);
 
+// 1.The mutex and condition variable are now contained within the threadsafe_queue instance, 
+// so separate variables are no longer required.
 threadsafe_queue<data_chunk> data_queue;
 
 void data_preparation_thread()
@@ -41,6 +45,7 @@ void data_preparation_thread()
     while(more_data_to_prepare())
     {
         data_chunk const data=prepare_data();
+        // 2.no external synchronization is required for the call to push()
         data_queue.push(data);
     }
 }
@@ -50,6 +55,7 @@ void data_processing_thread()
     while(true)
     {
         data_chunk data;
+        // 3.wait_and_pop() takes care of the condition variable wait
         data_queue.wait_and_pop(data);
         process(data);
         if(is_last_chunk(data))
