45ccbd25a6e70435acd67185329a64e2bc03a67e
Lazybones3 <31243810+Lazybones3@users.noreply.github.com>
2024-04-03 13:01:52 +0800

    Update listing_4.23.cpp

--- listings/listing_4.23.cpp
+++ listings/listing_4.23.cpp
@@ -1,3 +1,4 @@
+// Listing 4.23 Gathering results from futures using std::experimental::when_all
 std::experimental::future<FinalResult> process_data(
     std::vector<MyData>& vec)
 {
@@ -12,6 +13,8 @@ std::experimental::future<FinalResult> process_data(
         begin+=this_chunk_size;
     }
     return std::experimental::when_all(
+        // 1.you use when_all to wait for all the futures to become ready, 
+        // and then schedule the function using .then rather than async.
         results.begin(),results.end()).then(
         [](std::future<std::vector<
              std::experimental::future<ChunkResult>>> ready_results)
@@ -22,6 +25,8 @@ std::experimental::future<FinalResult> process_data(
             v.reserve(all_results.size());
             for(auto& f: all_results)
             {
+                // 2.the calls to get on the futures do not block, 
+                // as all the values are ready by the time execution gets there.
                 v.push_back(f.get());
             }
             return gather_results(v);
