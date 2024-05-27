6885bc426cc803061a6245bc5986e9da7efd066c
Lazybones3 <31243810+Lazybones3@users.noreply.github.com>
2024-05-27 12:55:33 +0800

    Update listing_2.1.cpp

--- listings/listing_2.1.cpp
+++ listings/listing_2.1.cpp
@@ -31,6 +31,7 @@ void oops()
     func my_func(some_local_state);
     // the new thread associated with my_thread will probably still be running when oops exits, 
     // because you’ve explicitly decided not to wait for it by calling detach().
+    // oops退出时my_thread可能还在运行，这时调用do_something(i)可能会访问已经销毁的变量
     std::thread my_thread(my_func);
     my_thread.detach();
 }
